7b0958bab495cc3f1fda399b52cb437f64bc27d4
Will Andre <wandre94@gmail.com>
2020-12-08 21:18:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiou]/i)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
