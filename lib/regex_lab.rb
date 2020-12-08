2f3a009a50dd74c26183311e8467202e8efedc04
Will Andre <wandre94@gmail.com>
2020-12-08 22:04:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/[0-9]{3}[0-9]{3}[0-9]{4}/)
+  if phone.match(/[0-9]{10}/)
     return true
   else
     return false
